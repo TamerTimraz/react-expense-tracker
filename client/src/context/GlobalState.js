daf9266df782032d799a552284831abe50ef60b0
TamerTimraz <tamertimraz04@gmail.com>
2024-01-27 15:26:13 -0500

    Added a getTransactions function to fetch the transactions from the database

--- client/src/context/GlobalState.js
+++ client/src/context/GlobalState.js
@@ -1,9 +1,11 @@
 import React, { createContext, useReducer } from "react";
 import AppReducer from "./AppReducer";
+import axios from "axios";
 
 // Initial state
 const initialState = {
   transactions: [],
+  error: null,
 };
 
 // Create context
@@ -14,6 +16,22 @@ export const GlobalProvider = ({ children }) => {
   const [state, dispatch] = useReducer(AppReducer, initialState);
 
   // Actions
+  async function getTransactions() {
+    try {
+      const res = await axios.get("/api/v1/transactions");
+
+      dispatch({
+        type: "GET_TRANSACTIONS",
+        payload: res.data.data,
+      });
+    } catch (err) {
+      dispatch({
+        type: "TRANSACTION_ERROR",
+        payload: err.response.data.error,
+      });
+    }
+  }
+
   function deleteTransaction(id) {
     dispatch({
       type: "DELETE_TRANSACTION",
@@ -32,6 +50,8 @@ export const GlobalProvider = ({ children }) => {
     <GlobalContext.Provider
       value={{
         transactions: state.transactions,
+        error: state.error,
+        getTransactions,
         deleteTransaction,
         addTransaction,
       }}
